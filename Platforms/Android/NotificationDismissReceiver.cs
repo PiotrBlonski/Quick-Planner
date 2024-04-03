2713b021010fd9e3ed11e2ffc67bb7e4da4b7d2f
Piotr Błonski <91888263+PiotrBlonski@users.noreply.github.com>
2024-04-03 11:08:40 +0200

    Add files via upload

---
+++ Platforms/Android/NotificationDismissReceiver.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Quick_Planner.Platforms.Android
+{
+    [BroadcastReceiver(Enabled = true, Exported = true)]
+    class NotificationDismissReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            AndroidServiceManager.Ringtone?.Stop();
+            AndroidServiceManager.Vibrator?.Cancel();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Quick_Planner
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
